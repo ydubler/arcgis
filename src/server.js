1e67bee976b2386d14d233a8276981a79fe46623
ydubler <switchgoof@gmail.com>
2020-08-04 14:18:14 -0600

    Displaying African cities with a population similar to Missoula

--- src/server.js
+++ src/server.js
@@ -89,10 +89,16 @@ server.get("/", (req, res) => {
           zoom: 3
         });
 
+
+
         // Cities feature layer
         var citiesLayer = new FeatureLayer({
         url:"https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/World_Cities/FeatureServer/0/",
         definitionExpression: "POP > 65000 AND POP < 85000",
+        source:  geometry: {
+          type: "polygon",
+          rings: [[[-29.28,16.38],[-18.52,35.10],[-5.6,35.94],[-5.09,35.98],[10.79,37.9],[33.99,31.52],[34.82,29.31],[34.54,27.39],[43.55,12.40],[55.01,13.21],[65.2817,-37.23],[6.81,-39.58]]]
+        },
         outFields: ["CITY_NAME", "POP"], // Return all fields so it can be queried client-side
         popupTemplate: {
           // Enable a popup
