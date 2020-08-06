0816deba609c0e34b33a821d6d0eb6202a7026f3
ydubler <switchgoof@gmail.com>
2020-08-06 09:16:52 -0600

    Displaying African cities with a population similar to Missoula

--- src/app/App.js
+++ src/app/App.js
@@ -25,6 +25,7 @@ function App() {
   return (
     <>
       <h1>ArcGIS API for Javascript</h1>
+      <h3>Please refresh the page if the map does not load.</h3>
       <div id="viewDiv"></div>
       {/* <img src="/public/images/coffee1.jpg"></img> */}
     </>
